f57b352da0002d46f1904bf30d61fc48049a5f41
BugHunter-X <askzenai@gmail.com>
2024-12-23 20:46:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+import Link from 'next/link';
+
+function MyComponent() {
+  return (
+    <div>
+      <Link href='/about'>About Us</Link>
+    </div>
+  );
+}
+
+export default MyComponent; 
+```
